c6b92cf69270bc0471241f44366a0c5fbd490b9b
lawdev <pachecolau27@gmail.com>
2026-03-09 21:03:33 -0500

    feat: setting up frontend with hono client rpc

---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    // tanstackRouter({
+    //   target: "react",
+    //   autoCodeSplitting: true,
+    // }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@server": path.resolve(__dirname, "../server")
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true
+      } 
+    }
+  }
+});
