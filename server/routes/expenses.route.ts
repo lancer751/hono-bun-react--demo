55f4eada38ad425959b5b6636c0e26242779579b
lawdev <pachecolau27@gmail.com>
2026-03-15 16:46:01 -0500

    feat: protecting expenses routes

--- server/routes/expenses.route.ts
+++ server/routes/expenses.route.ts
@@ -3,6 +3,7 @@ import { fakeExpenses, generateFakeExpenses } from "../helpers/randomExpenses";
 import { zValidator } from "@hono/zod-validator";
 import z from "zod";
 import { faker } from "@faker-js/faker";
+import { getUser } from "../kinde";
 
 // validate data structure in runtime to avoid receiving an incorrect structure from client
 
@@ -17,14 +18,14 @@ const createPostSchema = expenseSchema.omit({
 });
 
 export const expensesRoute = new Hono()
-  .get("/", async (c) => {
+  .get("/", getUser, async (c) => {
     if (fakeExpenses.length > 0) {
       return c.json({ expenses: fakeExpenses });
     }
     const expenses = generateFakeExpenses();
     return c.json({ expenses });
   })
-  .get("/total-spent", async (c) => {
+  .get("/total-spent", getUser, async (c) => {
     await new Promise((res) => setTimeout(res, 2000));
 
     if (fakeExpenses.length === 0) {
@@ -39,7 +40,7 @@ export const expensesRoute = new Hono()
 
     return c.json({ total: roundedTotal });
   })
-  .post("/", zValidator("json", createPostSchema), async (c) => {
+  .post("/",getUser,  zValidator("json", createPostSchema), async (c) => {
     const data = await c.req.valid("json");
 
     const expense = createPostSchema.parse(data);
@@ -48,7 +49,7 @@ export const expensesRoute = new Hono()
     return c.json(expense);
   })
   .get(
-    "/:id{[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}}",
+    "/:id{[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}}", getUser, 
     async (c) => {
       const id = c.req.param("id");
       const expense = fakeExpenses.find((exp) => exp.id === id);
@@ -61,7 +62,7 @@ export const expensesRoute = new Hono()
     },
   )
   .delete(
-    "/:id{[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}}",
+    "/:id{[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}}", getUser,
     async (c) => {
       const id = c.req.param("id");
 
